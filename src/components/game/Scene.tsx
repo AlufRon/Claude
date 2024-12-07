e429604269b0e12b99fcaf88f5162c6d21f84005
AlufRon <95473310+AlufRon@users.noreply.github.com>
2024-12-07 03:06:15 +0200

    Add Scene component for React Three Fiber

---
+++ src/components/game/Scene.tsx
@@ -0,0 +1,32 @@
+'use client'
+import { Canvas } from '@react-three/fiber'
+import { Environment, EffectComposer, Bloom } from '@react-three/drei'
+import { Physics } from '@react-three/rapier'
+import Table from './Table'
+import Ball from './Ball'
+import Paddle from './Paddle'
+
+export default function Scene() {
+  return (
+    <Canvas shadows camera={{ position: [4, 4, 4], fov: 50 }}>
+      <Environment preset="sunset" />
+      <ambientLight intensity={0.5} />
+      <directionalLight
+        position={[5, 5, 5]}
+        intensity={1}
+        castShadow
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+      />
+      <Physics>
+        <Table />
+        <Ball />
+        <Paddle position={[-1, 0.1, 0]} color="red" />
+        <Paddle position={[1, 0.1, 0]} color="blue" />
+      </Physics>
+      <EffectComposer>
+        <Bloom luminanceThreshold={1} intensity={2} />
+      </EffectComposer>
+    </Canvas>
+  )
+}
