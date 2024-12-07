9ba3daebba8f2957cde3f3d5ff8c0c9fadcf7494
AlufRon <95473310+AlufRon@users.noreply.github.com>
2024-12-07 03:23:25 +0200

    Enhance table with better materials and add lines

--- src/components/game/Table.tsx
+++ src/components/game/Table.tsx
@@ -1,16 +1,28 @@
-import { useGLTF } from '@react-three/drei'
+'use client'
 import { RigidBody } from '@react-three/rapier'
+import { MeshStandardMaterial } from 'three'
 
 export default function Table() {
   return (
-    <RigidBody type="fixed">
-      <mesh receiveShadow castShadow>
-        <boxGeometry args={[2.74, 0.05, 1.525]} />
-        <meshStandardMaterial color="#2f8f2f" />
-      </mesh>
+    <RigidBody type="fixed" colliders="cuboid">
+      <TableSurface />
       <TableLegs />
       <Net />
+      <TableLines />
     </RigidBody>
+  )
+}
+
+function TableSurface() {
+  return (
+    <mesh receiveShadow castShadow position={[0, 0, 0]}>
+      <boxGeometry args={[2.74, 0.05, 1.525]} />
+      <meshStandardMaterial 
+        color="#1a472a"
+        roughness={0.4}
+        metalness={0.1}
+      />
+    </mesh>
   )
 }
 
@@ -28,7 +40,11 @@ function TableLegs() {
       {positions.map((pos, i) => (
         <mesh key={i} position={pos} castShadow>
           <boxGeometry args={legGeometry} />
-          <meshStandardMaterial color="#333" />
+          <meshStandardMaterial 
+            color="#1a1a1a"
+            roughness={0.3}
+            metalness={0.7}
+          />
         </mesh>
       ))}
     </>
@@ -37,9 +53,36 @@ function TableLegs() {
 
 function Net() {
   return (
-    <mesh position={[0, 0.1, 0]}>
+    <mesh position={[0, 0.1, 0]} castShadow>
       <boxGeometry args={[0.02, 0.15, 1.525]} />
-      <meshStandardMaterial color="white" opacity={0.7} transparent />
+      <meshStandardMaterial 
+        color="white"
+        opacity={0.9}
+        transparent
+        roughness={0.5}
+        metalness={0.1}
+      />
     </mesh>
+  )
+}
+
+function TableLines() {
+  return (
+    <>
+      {/* Center line */}
+      <mesh position={[0, 0.026, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[0.02, 1.525]} />
+        <meshStandardMaterial color="white" />
+      </mesh>
+      {/* Border lines */}
+      <mesh position={[0, 0.026, 0.7625]} rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[2.74, 0.02]} />
+        <meshStandardMaterial color="white" />
+      </mesh>
+      <mesh position={[0, 0.026, -0.7625]} rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry args={[2.74, 0.02]} />
+        <meshStandardMaterial color="white" />
+      </mesh>
+    </>
   )
 }
