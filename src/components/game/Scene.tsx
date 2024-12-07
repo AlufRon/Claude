3fac97f2baf46097c15a90b18ae15e7dc71dddd4
AlufRon <95473310+AlufRon@users.noreply.github.com>
2024-12-07 03:26:07 +0200

    Update Scene with enhanced visuals and game state integration

--- src/components/game/Scene.tsx
+++ src/components/game/Scene.tsx
@@ -1,16 +1,31 @@
 'use client'
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { Environment, OrbitControls, PerspectiveCamera, Stars } from '@react-three/drei'
-import { EffectComposer, Bloom, ChromaticAberration, DepthOfField } from '@react-three/postprocessing'
+import { 
+  Environment, 
+  OrbitControls, 
+  PerspectiveCamera, 
+  Stars,
+  KeyboardControls
+} from '@react-three/drei'
+import { 
+  EffectComposer, 
+  Bloom, 
+  ChromaticAberration, 
+  DepthOfField,
+  Vignette
+} from '@react-three/postprocessing'
 import { BlendFunction } from 'postprocessing'
 import { Physics } from '@react-three/rapier'
 import { Ground, RoomLights, Walls } from './Environment'
 import Table from './Table'
 import Ball from './Ball'
 import Paddle from './Paddle'
+import { useGameStore } from '@/store/gameStore'
 
 export default function Scene() {
+  const { isPlaying, isPaused } = useGameStore()
+
   return (
     <Canvas shadows>
       <PerspectiveCamera
@@ -32,24 +47,29 @@ export default function Scene() {
           factor={4}
           saturation={0}
           fade
+          speed={1}
         />
         
         <Environment preset="night" />
         <RoomLights />
         
-        <Physics debug>
+        <Physics 
+          debug={false}
+          gravity={[0, -9.81, 0]}
+          paused={!isPlaying || isPaused}
+        >
           <Ground />
           <Walls />
           <Table />
           <Ball />
-          <Paddle position={[-1, 0.1, 0]} color="#ff4040" />
-          <Paddle position={[1, 0.1, 0]} color="#4040ff" />
+          <Paddle position={[-1, 0.1, 0]} color="#ff4040" playerId={1} />
+          <Paddle position={[1, 0.1, 0]} color="#4040ff" playerId={2} />
         </Physics>
         
         <EffectComposer>
           <Bloom 
-            intensity={1}
-            luminanceThreshold={0.8}
+            intensity={1.5}
+            luminanceThreshold={0.6}
             luminanceSmoothing={0.9}
           />
           <ChromaticAberration
@@ -62,6 +82,10 @@ export default function Scene() {
             bokehScale={2}
             height={480}
           />
+          <Vignette
+            opacity={0.3}
+            darkness={0.8}
+          />
         </EffectComposer>
         
         <OrbitControls 
@@ -70,6 +94,9 @@ export default function Scene() {
           maxDistance={10}
           minPolarAngle={Math.PI / 4}
           maxPolarAngle={Math.PI / 2}
+          target={[0, 0, 0]}
+          enableDamping
+          dampingFactor={0.05}
         />
       </Suspense>
     </Canvas>
