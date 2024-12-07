2c995fd6042720b5f0b799e4497a2e8a2522f449
AlufRon <95473310+AlufRon@users.noreply.github.com>
2024-12-07 03:09:16 +0200

    Add loading state and Suspense to fix hydration issues

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -1,12 +1,18 @@
 'use client'
+import { Suspense } from 'react'
 import dynamic from 'next/dynamic'
 
-const Scene = dynamic(() => import('./Scene'), { ssr: false })
+const Scene = dynamic(() => import('./Scene'), {
+  ssr: false,
+  loading: () => <div className="w-full h-[600px] bg-black" />
+})
 
 export default function Game() {
   return (
     <div className="w-full h-[600px] relative">
-      <Scene />
+      <Suspense fallback={<div className="w-full h-[600px] bg-black" />}>
+        <Scene />
+      </Suspense>
     </div>
   )
 }
