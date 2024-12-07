eb8ab7de08f92cd3b7a03b275dfb2a6a67c895ea
AlufRon <95473310+AlufRon@users.noreply.github.com>
2024-12-07 04:02:22 +0200

    Update scene with improved lighting and post-processing

--- src/components/game/Scene.tsx
+++ src/components/game/Scene.tsx
@@ -1,9 +1,9 @@
 'use client'
 import { Canvas } from '@react-three/fiber'
 import { Environment, OrbitControls, Stars } from '@react-three/drei'
-import { EffectComposer, Bloom } from '@react-three/postprocessing'
+import { EffectComposer, Bloom, SMAA } from '@react-three/postprocessing'
 import { Physics } from '@react-three/rapier'
-import { Ground, RoomLights, Walls } from './Environment'
+import { Room } from './Environment'
 import Table from './Table'
 import Ball from './Ball'
 import Paddle from './Paddle'
@@ -16,47 +16,46 @@ export default function Scene() {
     <div style={{ width: '100vw', height: '100vh' }}>
       <Canvas
         shadows
-        camera={{ position: [4, 4, 4], fov: 60 }}
+        camera={{ position: [4, 4, 4], fov: 50 }}
         gl={{
-          antialias: true,
+          antialias: false, // We'll use SMAA instead
           powerPreference: 'high-performance',
         }}
       >
         <color attach="background" args={["#000000"]} />
         
-        <ambientLight intensity={0.5} />
-        <directionalLight
-          position={[5, 5, 5]}
-          intensity={1}
-          castShadow
-          shadow-mapSize={[2048, 2048]}
-        />
+        {/* Main ambient light */}
+        <ambientLight intensity={0.2} />
+        
+        {/* Environment maps for reflections */}
+        <Environment preset="apartment" background blur={1} />
+        
+        {/* Room with spotlights */}
+        <Room />
         
         <Stars
           radius={100}
           depth={50}
-          count={5000}
+          count={1000}
           factor={4}
           saturation={0}
           fade
         />
-
-        <Environment preset="night" />
 
         <Physics
           debug={false}
           gravity={[0, -9.81, 0]}
           paused={!isPlaying || isPaused}
         >
-          <Ground />
-          <Walls />
           <Table />
           <Ball />
           <Paddle position={[-1, 0.3, 0]} color="#ff4040" playerId={1} />
           <Paddle position={[1, 0.3, 0]} color="#4040ff" playerId={2} />
         </Physics>
 
-        <EffectComposer disableNormalPass>
+        {/* Post processing */}
+        <EffectComposer multisampling={0}>
+          <SMAA /> {/* Better anti-aliasing */}
           <Bloom
             intensity={0.5}
             luminanceThreshold={0.9}
@@ -64,12 +63,16 @@ export default function Scene() {
           />
         </EffectComposer>
 
+        {/* Camera controls */}
         <OrbitControls
           enablePan={false}
           minDistance={3}
           maxDistance={10}
           minPolarAngle={Math.PI / 4}
           maxPolarAngle={Math.PI / 2}
+          target={[0, 0, 0]}
+          enableDamping
+          dampingFactor={0.05}
         />
       </Canvas>
     </div>
