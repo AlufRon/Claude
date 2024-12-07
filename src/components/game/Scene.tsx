6da6ff9afb98d09926e3f6823770bfeaefc0085a
AlufRon <95473310+AlufRon@users.noreply.github.com>
2024-12-07 04:07:00 +0200

    Update scene to work with new outdoor environment

--- src/components/game/Scene.tsx
+++ src/components/game/Scene.tsx
@@ -18,30 +18,17 @@ export default function Scene() {
         shadows
         camera={{ position: [4, 4, 4], fov: 50 }}
         gl={{
-          antialias: false, // We'll use SMAA instead
+          antialias: false,
           powerPreference: 'high-performance',
         }}
       >
-        <color attach="background" args={["#000000"]} />
+        <color attach="background" args={["#87ceeb"]} /> {/* Sky blue background */}
         
-        {/* Main ambient light */}
-        <ambientLight intensity={0.2} />
+        {/* Environment for better lighting */}
+        <Environment preset="sunset" background blur={0.6} />
         
-        {/* Environment maps for reflections */}
-        <Environment preset="apartment" background blur={1} />
-        
-        {/* Room with spotlights */}
         <Room />
         
-        <Stars
-          radius={100}
-          depth={50}
-          count={1000}
-          factor={4}
-          saturation={0}
-          fade
-        />
-
         <Physics
           debug={false}
           gravity={[0, -9.81, 0]}
@@ -53,9 +40,8 @@ export default function Scene() {
           <Paddle position={[1, 0.3, 0]} color="#4040ff" playerId={2} />
         </Physics>
 
-        {/* Post processing */}
         <EffectComposer multisampling={0}>
-          <SMAA /> {/* Better anti-aliasing */}
+          <SMAA />
           <Bloom
             intensity={0.5}
             luminanceThreshold={0.9}
@@ -63,13 +49,12 @@ export default function Scene() {
           />
         </EffectComposer>
 
-        {/* Camera controls */}
         <OrbitControls
           enablePan={false}
           minDistance={3}
-          maxDistance={10}
-          minPolarAngle={Math.PI / 4}
-          maxPolarAngle={Math.PI / 2}
+          maxDistance={20}
+          minPolarAngle={Math.PI / 6}
+          maxPolarAngle={Math.PI / 2.5}
           target={[0, 0, 0]}
           enableDamping
           dampingFactor={0.05}
