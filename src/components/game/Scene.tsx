fb09cec2abd625d19d335a59ad0902442970fbbd
AlufRon <95473310+AlufRon@users.noreply.github.com>
2024-12-07 11:00:30 +0200

    Fix Scene component with simplified imports and setup

--- src/components/game/Scene.tsx
+++ src/components/game/Scene.tsx
@@ -1,22 +1,7 @@
 'use client'
 import { Canvas } from '@react-three/fiber'
-import { 
-  Environment as DreiEnvironment,
-  OrbitControls,
-  Fog,
-  Stars,
-  BakeShadows
-} from '@react-three/drei'
-import { 
-  EffectComposer,
-  Bloom,
-  SMAA,
-  HueSaturation,
-  Vignette,
-  DepthOfField
-} from '@react-three/postprocessing'
+import { Environment, OrbitControls, Stars } from '@react-three/drei'
 import { Physics } from '@react-three/rapier'
-import { Environment } from './Environment'  // Updated import
 import Table from './Table'
 import Ball from './Ball'
 import Paddle from './Paddle'
@@ -31,27 +16,23 @@ export default function Scene() {
         shadows
         camera={{ position: [8, 8, 8], fov: 50 }}
         gl={{
-          antialias: false,
-          alpha: false,
-          physicallyCorrectLights: true,
+          antialias: true,
           shadowMap: {
-            enabled: true,
-            type: 3
-          },
+            enabled: true
+          }
         }}
       >
         <color attach="background" args={["#87ceeb"]} />
         
-        {/* Environment map */}
-        <DreiEnvironment
-          preset="sunset"
-          ground={{
-            height: 15,
-            radius: 60,
-            scale: 15
-          }}
+        {/* Environment setup */}
+        <Environment preset="sunset" />
+        <ambientLight intensity={0.5} />
+        <directionalLight
+          position={[10, 10, 10]}
+          intensity={1}
+          castShadow
+          shadow-mapSize={[2048, 2048]}
         />
-        <Fog color="#b0c4de" near={1} far={100} />
         
         <Stars
           radius={100}
@@ -60,10 +41,7 @@ export default function Scene() {
           factor={4}
           saturation={0}
           fade
-          speed={0.5}
         />
-        
-        <Environment />
         
         <Physics
           debug={false}
@@ -76,41 +54,12 @@ export default function Scene() {
           <Paddle position={[1, 0.3, 0]} color="#4040ff" playerId={2} />
         </Physics>
 
-        <EffectComposer multisampling={0}>
-          <SMAA />
-          <Bloom
-            intensity={0.5}
-            luminanceThreshold={0.8}
-            luminanceSmoothing={0.9}
-          />
-          <DepthOfField
-            focusDistance={0}
-            focalLength={0.02}
-            bokehScale={2}
-            height={480}
-          />
-          <HueSaturation
-            hue={0}
-            saturation={0.15}
-          />
-          <Vignette
-            offset={0.5}
-            darkness={0.5}
-            opacity={0.3}
-          />
-        </EffectComposer>
-
-        <BakeShadows />
-
         <OrbitControls
           enablePan={false}
           minDistance={5}
           maxDistance={25}
           minPolarAngle={Math.PI / 6}
           maxPolarAngle={Math.PI / 2.5}
-          target={[0, 0, 0]}
-          enableDamping
-          dampingFactor={0.05}
         />
       </Canvas>
     </div>
